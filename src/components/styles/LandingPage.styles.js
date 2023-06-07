fd97a9fddfcd8710580350f03370ea1a8f655727
Ammar Sarwar <106272639+ammarsarwar@users.noreply.github.com>
2023-06-07 15:04:44 +0500

    Delete src directory

--- src/components/styles/LandingPage.styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const EditorContainer = styled.div`
-  width: 100%;
-`;
--- src/pages/DashboardLayout.jsx
+++
@@ -1,7 +0,0 @@
-import Layout from "../components/DashboardComponents/Layout";
-
-const Dashboard = () => {
-  return <Layout />;
-};
-
-export default Dashboard;
